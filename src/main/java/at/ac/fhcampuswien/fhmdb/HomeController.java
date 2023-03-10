ab69f3b8986b15013df847e261900038af557c4d
Hamza <43032584+ritopsis@users.noreply.github.com>
2023-03-10 15:32:56 +0100

    Added sorting Movies ascending/descending

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import at.ac.fhcampuswien.fhmdb.ui.MovieCell;
 import com.jfoenix.controls.JFXButton;
@@ -12,6 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -38,13 +41,13 @@ public class HomeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         observableMovies.addAll(allMovies);         // add dummy data to observable list
-
         // initialize UI stuff
         movieListView.setItems(observableMovies);   // set data of observable list to list view
         movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
 
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
         genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().addAll(Genre.values()); //add all genres to combobox
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
@@ -52,14 +55,34 @@ public class HomeController implements Initializable {
         // Sort button example:
         sortBtn.setOnAction(actionEvent -> {
             if(sortBtn.getText().equals("Sort (asc)")) {
-                // TODO sort observableMovies ascending
+                // TODO sort observableMovies ascending a-z
+                sortMoviesAscending(observableMovies);
                 sortBtn.setText("Sort (desc)");
             } else {
-                // TODO sort observableMovies descending
+                // TODO sort observableMovies descending z-a
+                sortMoviesDescending(observableMovies);
                 sortBtn.setText("Sort (asc)");
             }
         });
 
 
     }
+    public void sortMoviesAscending(ObservableList<Movie> allMovies) {
+        allMovies.sort(Comparator.comparing(Movie::getTitle));
+
+        //First attempt
+        /*
+        Comparator<Movie> titlecomapre = new Comparator<Movie>() { //create Comparator for collection.sort
+            @Override //is abstact methode in Comparator -> needs to be overrided
+            public int compare(Movie m1, Movie m2) {
+                return String.CASE_INSENSITIVE_ORDER.compare(m1.getTitle(), m2.getTitle());
+            }
+        };
+        Collections.sort(allMovies, titlecomapre);
+        */
+    }
+    public void sortMoviesDescending(ObservableList<Movie> allMovies) {
+        allMovies.sort(Comparator.comparing(Movie::getTitle).reversed());
+    }
+
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -12,9 +12,8 @@ import javafx.scene.paint.Color;
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
-
     private final Label genres = new Label();
-    private final VBox layout = new VBox(title, detail,genres);
+    private final VBox layout = new VBox(title, detail, genres);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -30,17 +29,23 @@ public class MovieCell extends ListCell<Movie> {
                             ? movie.getDescription()
                             : "No description available"
             );
-            genres.setText(movie.getGenres());
+            genres.setText(
+                    movie.getGenres() != null
+                    ? movie.getGenres()
+                    : "No genre available"
+            );
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
+            genres.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
             detail.setMaxWidth(this.getScene().getWidth() - 30);
             detail.setWrapText(true);
+            genres.fontProperty().set(genres.getFont().font(13));
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
