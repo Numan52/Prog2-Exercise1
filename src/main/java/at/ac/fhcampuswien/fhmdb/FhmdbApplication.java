56b2ad8cda7b299807717bf7e939c3efa7ecad4e
FH-Sina <sina.alizadeh@stud.fh-campuswien.ac.at>
2023-06-10 16:50:03 +0200

    FXMLLoader_and_MyFactory_creating_in_FhmdbApplication

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -3,6 +3,7 @@ package at.ac.fhcampuswien.fhmdb;
 
 import at.ac.fhcampuswien.fhmdb.database.Database;
 import javafx.application.Application;
+import at.ac.fhcampuswien.fhmdb.patterns.MyFactory;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
@@ -17,6 +18,8 @@ public class FhmdbApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FhmdbApplication.class.getResource("home-view.fxml"));
+        MyFactory myFactory = new MyFactory();
+        fxmlLoader.setControllerFactory(myFactory);
         Scene scene = new Scene(fxmlLoader.load(), 890, 620);
         scene.getStylesheets().add(Objects.requireNonNull(FhmdbApplication.class.getResource("styles.css")).toExternalForm());
         stage.setTitle("FHMDb");
